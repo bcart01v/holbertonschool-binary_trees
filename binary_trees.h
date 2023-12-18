479c295029b827ed05df44714bfb3c1f5aa93219
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-12-18 15:22:43 -0600

    More Counting

--- binary_trees.h
+++ binary_trees.h
@@ -74,4 +74,7 @@ size_t binary_tree_size(const binary_tree_t *tree);
 /*Sometimes it's nice to sit and count the leaves */
 size_t binary_tree_leaves(const binary_tree_t *tree);
 
+/*Count the nodes with at least 1 child*/
+size_t binary_tree_nodes(const binary_tree_t *tree);
+
 #endif
