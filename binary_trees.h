46367d5775bd750ab6049f58802efff6c30c57b5
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-12-17 22:38:41 -0600

    Leaf checker

--- binary_trees.h
+++ binary_trees.h
@@ -47,6 +47,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 /*Deletes an entire binary tree (Use with caution)*/
 void binary_tree_delete(binary_tree_t *tree);
 
-
+/*Checks if node is leaf.*/
+int binary_tree_is_leaf(const binary_tree_t *node);
 
 #endif
